aed742726cedb02ee14352d1bec7af2828d3879b
vagrant <vagrant@vagrant.vm>
2018-03-01 07:58:50 +0000

    add controller views static files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # koa2-base
+
+## 目录结构
+```tree
+  ├── controller/
+  │     ├── home.js
+  ├── service/
+  │     ├── home.js
+  ├── views/
+  │     ├── common/ 
+  │         ├── header.html
+  │         ├── footer.html
+  │         ├── layout.html
+  │         ├── layout-home.html
+  │     ├── home/ 
+  │         ├── index.html
+  │         ├── login.html
+  │         ├── success.html
+  ├── public/ 
+  │     ├──  css/
+  │         ├── common.css 
+  ├── app.js
+  ├── router.js
+  ├── package.json
+ ``` 
--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const Koa = require('koa')
-// 注意 require('koa-router') 返回的是函数:
-const router = require('koa-router')()
+
+// 引入请求体解析中间件
 const bodParser = require("koa-bodyparser")
+
+//引入视图引擎
+const nunjucks = require('koa-nunjucks-2')
+
+// 引入 koa-static
+const staticFiles = require('koa-static')
 const app = new Koa()
 
 // 添加路由
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>index page</h1>`
-})
+const router = require('./router')
+const path = require("path");
+// 指定 public目录为静态资源目录，用来存放 js css /koa2/images 等
+app.use(staticFiles(path.resolve(__dirname, "./public/")))
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),// 指定视图目录
+    nunjucksConfig: {
+        trimBlocks: true // 开启转义 防Xss
+    }
+}))
 
-router.get('/home', async (ctx, next) => {
-    console.log(ctx.request.query)
-    console.log(ctx.request.querystring)
-    ctx.response.body = '<h1>HOME page</h1>'
-})
-
-router.get('/404', async (ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-})
-
-// 调用路由中间件
-app.use(router.routes())
 app.use(bodParser())
-
-app.listen(3001, ()=>{
-    console.log('server is running at http://localhost:3000')
-})
+router(app)
 
 app.listen(3000, ()=>{
     console.log('server is running at http://localhost:3000')
