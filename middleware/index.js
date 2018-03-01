6bdae084fd99ca302c706004d1a0f67386e66c15
kiwi <kiwi@kiwis-iMac.lan>
2018-03-02 00:12:20 +0800

    error handling

--- middleware/index.js
+++ middleware/index.js
@@ -5,8 +5,13 @@ const staticFiles = require('koa-static')
 
 const miSend = require('./mi-send')
 const miLog = require('./mi-log')
+// 引入请求错误中间件
+const miHttpError = require('./mi-http-error')
 module.exports = (app) => {
-
+    // 应用请求错误中间件
+    app.use(miHttpError({
+        errorPageFolder: path.resolve(__dirname, '../errorPage')
+    }))
     app.use(miLog())
     app.use(staticFiles(path.resolve(__dirname, "../public")))
 
@@ -20,5 +25,17 @@ module.exports = (app) => {
 
     app.use(bodyParser())
     app.use(miSend())
+
+    // 增加错误的监听处理
+    app.on("error", (err, ctx) => {
+        if (ctx && !ctx.headerSent && ctx.status < 500) {
+            ctx.status = 500
+        }
+        if (ctx && ctx.log && ctx.log.error) {
+            if (!ctx.state.logged) {
+                ctx.log.error(err.stack)
+            }
+        }
+    })
 
 }
